5fa53f30f238690fc7252fe0d2f706cb7f86a1f5
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-10-31 21:21:22 -0500

    (feature): Filter

---
+++ Frontend/src/components/Filter.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+const categories = ["Todo", "Artista", "Álbumes", "Canciones"];
+
+function Filter() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleChangeFilter = (index:number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="gap-2 flex items-center">
+      {categories.map((category, index) => (
+        <button 
+          key={index}
+          onClick={() => handleChangeFilter(index)}
+          className={`px-4 py-2 rounded-full transition-all duration-300 ease-in-out ${currentIndex === index ? 'bg-primary ring-2 ring-primary' : 'bg-card-foreground'}`}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default Filter
