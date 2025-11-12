e3e5f22078e1cbdf602e3c79370ce6622a5ba954
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-11-11 20:44:15 -0500

    (feature): Tracks

--- Frontend/src/models/Track.tsx
+++ Frontend/src/models/Track.tsx
@@ -1,9 +1,3 @@
-import { CustomDropdownMenu } from "@/components/CustomDropdownMenu";
-import { Button } from "@/components/ui/button";
-import icons from "@/constants/icons";
-import type { ColumnDef } from "@tanstack/react-table";
-import { Timer } from "lucide-react";
-
 export interface Track {
   id: string;
   title: string;
